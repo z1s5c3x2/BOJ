c8e9362b931b432c22f4576c6a741b145ed92032
z1s5c3x2 <z1s5c3x2@gmail.com>
2024-02-15 19:55:23 +0900

    뒤집기 그리드

--- 그리디/1439.py
+++ 그리디/1439.py
@@ -1,3 +1,8 @@
-_str = input().count
+_str = input()
 
-print(_str)
+list1 = [_str.split('0'),_str.split('1')]
+
+list1[0] = len(list1[0]) - list1[0].count('')
+list1[1] = len(list1[1]) - list1[1].count('')
+
+print(min(list1))

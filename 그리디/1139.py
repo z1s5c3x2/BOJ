435601d3949f74621489f0646e5bee87c2b10a6d
z1s5c3x2 <z1s5c3x2@gmail.com>
2024-02-28 21:53:59 +0900

    단어 수학

---
+++ 그리디/1139.py
@@ -0,0 +1,16 @@
+list1 = [ input() for _ in range(int(input()))]
+#tmp = {'A':0,'B':0,'C':0,'D':0,'E':0,'F':0,'G':0,'H':0,'I':0,'J':0,}
+tmp = {}
+for x in list1:
+    ln = len(x)
+    for i,a in enumerate(x):
+        if a in tmp:
+            tmp[a] += 10**(ln-(i+1))
+        else:
+            tmp[a] = 10**(ln-(i+1))
+rnk = 9
+for x in sorted(tmp.items(), key= lambda x: x[1], reverse=True):
+    for y,z in enumerate(list1):
+        list1[y] = z.replace(x[0],str(rnk))
+    rnk -=1
+print(sum(list(map(int,list1))))

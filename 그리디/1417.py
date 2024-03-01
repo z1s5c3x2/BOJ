1520b1b80cf242fd72b6cebc4539ae0d2c54dfab
z1s5c3x2 <z1s5c3x2@gmail.com>
2024-03-01 23:57:45 +0900

    국회의원 선거

---
+++ 그리디/1417.py
@@ -0,0 +1,17 @@
+n = int(input())
+t = int(input())
+list1 = sorted([int(input()) for _ in range(n-1)] , reverse=True)
+cnt = 0
+
+if n == 1:
+    print(0)
+
+else:
+
+    while list1[0] >= t:
+        t += 1
+        list1[0] -= 1
+        cnt += 1
+        list1.sort(reverse=True)
+        
+    print(cnt)

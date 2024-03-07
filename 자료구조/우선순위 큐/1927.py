df6c39933418b5847d239a232f370c714fc406bc
z1s5c3x2 <z1s5c3x2@gmail.com>
2024-03-07 20:56:53 +0900

    폴더 이동

---
+++ 자료구조/우선순위 큐/1927.py
@@ -0,0 +1,18 @@
+import heapq
+import sys
+input = sys.stdin.readline
+hq = []
+answer = []
+for _ in range(int(input())):
+    n = int(input())
+    if  n == 0:
+        if len(hq) == 0:
+            answer.append(0)
+        else:
+            answer.append(heapq.heappop(hq))    
+    else:
+        heapq.heappush(hq,n)
+    
+
+print(*answer,sep='\n')
+

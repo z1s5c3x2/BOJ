b2bf811e3dd99a5d11c8c3cbe122f069ee3dbcbc
z1s5c3x2 <z1s5c3x2@gmail.com>
2024-03-15 19:30:01 +0900

    거스름돈

---
+++ DP/14916.py
@@ -0,0 +1,16 @@
+n = int(input())
+cnt = 0
+
+while True:
+    if n % 5 == 0:
+        cnt += n//5
+        break
+    else:
+        n -=2
+        cnt +=1
+    
+    if n < 0:
+        cnt = -1
+        break
+
+print(cnt)
